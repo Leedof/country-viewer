4f34991bb1a65b41d9d2e92711a8b37ef10d2c01
Leedof <m4kushchenko@gmail.com>
2023-03-25 18:10:26 +0200

    feat: fetch, store, show data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Controls } from "./components/Controls";
+import { ALL_COUNTRIES } from "./config";
+import { List } from "./components/List";
+import { Card } from "./components/Card";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  console.log(countries);
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+  }, []);
   return (
     <>
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((country) => {
+            const countryInfo = {
+              img: country.flags.png,
+              name: country.name.official,
+              info: [
+                {
+                  title: "Population",
+                  description: country.population.toLocaleString(),
+                },
+                {
+                  title: "Region",
+                  description: country.region,
+                },
+                {
+                  title: "Capital",
+                  description: country.capital,
+                },
+              ],
+            };
+            return <Card key={country.name.official} {...countryInfo} />;
+          })}
+        </List>
       </Main>
     </>
   );
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const BASE_URL = "https://restcountries.com/v3.1/";
+
+export const ALL_COUNTRIES =
+  BASE_URL + "all?fields=name,capital,flags,population,region";
+
+export const searchByCountry = (countryName) =>
+  BASE_URL + "name/" + countryName;
+
+export const filterByCode = (codes) =>
+  BASE_URL + "alpha?codes=" + codes.join(",");
