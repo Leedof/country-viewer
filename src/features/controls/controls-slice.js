1fa71bc0996f2100c017548dae89e707958c9504
Leedof <m4kushchenko@gmail.com>
2023-03-27 18:51:37 +0300

    feat: create controls slice

---
+++ src/features/controls/controls-slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  search: '',
+  region: '',
+};
+
+const controlsSlice = createSlice({
+  name: '@@controls',
+  initialState,
+  reducers: {
+    setSearch(state, action) {
+      state.search = action.payload;
+    },
+    setRegion(state, action) {
+      state.region = action.payload;
+    },
+  },
+});
+
+export const controlsReducer = controlsSlice.reducer;
+export const { setRegion, setSearch } = controlsSlice.actions;
