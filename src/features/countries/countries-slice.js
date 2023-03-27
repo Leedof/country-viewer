6b06f8f2144fd15a8a17631aa07eba098a71fa26
Leedof <m4kushchenko@gmail.com>
2023-03-27 17:17:48 +0300

    feat: create countries slice

--- src/features/countries/countries-slice.js
+++ src/features/countries/countries-slice.js
@@ -1,13 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchCountries = createAsyncThunk(
+  '@@countries/fetch-countries',
+  (_, { extra: { client, api } }) => {
+    return client.get(api.ALL_COUNTRIES);
+  },
+  {
+    condition: (_, { getState }) => {
+      const {
+        countries: { status },
+      } = getState();
+      if (status === 'loading') return false;
+    },
+  }
+);
 
 const initialState = {
-  countries: [],
+  status: 'idle', // idle, loading, rejected, received
+  error: null,
+  list: [],
 };
 
 const countriesSlice = createSlice({
   name: '@@countries',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCountries.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(fetchCountries.rejected, (state, action) => {
+        state.status = 'rejected';
+        state.error = action.payload || action.meta.error;
+      })
+      .addCase(fetchCountries.fulfilled, (state, action) => {
+        state.status = 'received';
+        state.list = action.payload.data;
+      });
+  },
 });
 
 export const countriesReducer = countriesSlice.reducer;
+
+//Selectors
+
+export const selectCountriesInfo = (state) => ({
+  status: state.countries.status,
+  error: state.countries.error,
+  qty: state.countries.list.length,
+});
+export const selectAllCountries = (state) => state.countries.list;
+
+// export const selectFilteredCountries ;
